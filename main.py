4f8079fa31c91dd2c1f9932b7f395aebb2947618
PratikW31 <112165801+PratikW31@users.noreply.github.com>
2022-09-27 21:23:10 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,44 @@
 import streamlit as st
-import numpy as np
+
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 
-st.title("Pratik Wable")
-
-st.write("MSC DSAI")
-
-st.header("PYTHON")
-
+st.title("Pratik")
+st.write("MscDSAI")
+st.header("Python")
 st.latex(r'''
     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
     \sum_{k=0}^{n-1} ar^k =
     a \left(\frac{1-r^{n}}{1-r}\right)
     ''')
+st.caption('This is a string that explains something above.')
+st.text('This is some text.')
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+st.code(code, language='python')
+df = pd.DataFrame(
+   np.random.randn(10, 5),
+   columns=('col %d' % i for i in range(5)))
 
-st.caption("ABCBCBCBCC")
-
-
+st.table(df)
 chart_data = pd.DataFrame(
     np.random.randn(20, 3),
     columns=['a', 'b', 'c'])
 
-
 st.line_chart(chart_data)
-
 chart_data = pd.DataFrame(
     np.random.randn(20, 3),
     columns=['a', 'b', 'c'])
 
 st.area_chart(chart_data)
-
 chart_data = pd.DataFrame(
     np.random.randn(50, 3),
     columns=["a", "b", "c"])
 
 st.bar_chart(chart_data)
+
 
 arr = np.random.normal(1, 1, size=100)
 fig, ax = plt.subplots()
@@ -44,4 +46,16 @@ ax.hist(arr, bins=20)
 
 st.pyplot(fig)
 
+col1, col2, col3 = st.columns(3)
 
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
